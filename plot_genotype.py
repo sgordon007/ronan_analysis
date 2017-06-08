27a94ae647ad10fa25f97c5603ff34009d7a8caf
sgordon007 <seangordon07@gmail.com>
2017-06-08 15:23:00 -0700

    made multiplot with matplotlib on trio genotype samples

--- plot_genotype.py
+++ plot_genotype.py
@@ -2,59 +2,41 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 df = pd.read_csv('BhD1.BTNCA.11436.8.207067.ACTCGCT-TATCCTC.fastq_calling.bed.bedgraph', sep='\t', header=None, lineterminator='\n')
 df.columns = ['chr', 'start', 'stop', 'UCXX', 'PNWH', 'HET']
 test_data = df.head()
 print test_data
 
-
 to_subset = ['UCXX', 'PNWH', 'HET']
-
 df2 = df[to_subset]
 
-
 UCXX = (df['start'], df['UCXX'])
-
 PNWH = (df['start'], df['PNWH'])
-
 HET = (df['start'], df['HET'])
 
 data = (UCXX, PNWH, HET)
 colors = ("red", "green", "blue")
 groups = ("UCXX", "PNWH", "HET")
 
-# Create plot
+# Create a area plot
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1, axisbg="1.0")
-
-### this plot scatterplot OK
-# for data, color, group in zip(data, colors, groups):
-#     x, y = data
-#     ax.scatter(x, y, alpha=0.8, c=color, edgecolors='none', s=30, label=group)
-#
-# plt.title('Matplot scatter plot')
-# plt.legend(loc=2)
-# plt.show()
-
-#
-# for data, color, group in zip(data, colors, groups):
-#     x, y = data
-#     ax.scatter(x, y, alpha=0.8, c=color, edgecolors='none', s=30, label=group)
-#
-# plt.title('Matplot scatter plot')
-# plt.legend(loc=2)
-# plt.show()
-
 df2.plot(kind='area', stacked=False)
 plt.show()
 
 
-# g2 = (0.4 + 0.3 * np.random.rand(N), 0.5 * np.random.rand(N))
-# g3 = (0.3 * np.random.rand(N), 0.3 * np.random.rand(N))
+# ### this alternative plot scatterplot OK
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 1, 1, axisbg="1.0")
+# for data, color, group in zip(data, colors, groups):
+#     x, y = data
+#     ax.scatter(x, y, alpha=0.8, c=color, edgecolors='none', s=30, label=group)
 #
-# data = (g1, g2, g3)
-# colors = ("red", "green", "blue")
-# groups = ("coffee", "tea", "water")
+# plt.title('Matplot scatter plot')
+# plt.legend(loc=2)
+# plt.show()
+
 
 
 
@@ -86,31 +68,3 @@ plt.show()
 # for N in l1:
 #     print l1[0,1]
 
-#
-
-
-#
-# import numpy as np
-# import matplotlib.pyplot as plt
-#
-# # Create data
-# N = 60
-# g1 = (0.6 + 0.6 * np.random.rand(N), np.random.rand(N))
-# g2 = (0.4 + 0.3 * np.random.rand(N), 0.5 * np.random.rand(N))
-# g3 = (0.3 * np.random.rand(N), 0.3 * np.random.rand(N))
-#
-# data = (g1, g2, g3)
-# colors = ("red", "green", "blue")
-# groups = ("coffee", "tea", "water")
-#
-# # Create plot
-# fig = plt.figure()
-# ax = fig.add_subplot(1, 1, 1, axisbg="1.0")
-#
-# for data, color, group in zip(data, colors, groups):
-#     x, y = data
-#     ax.scatter(x, y, alpha=0.8, c=color, edgecolors='none', s=30, label=group)
-#
-# plt.title('Matplot scatter plot')
-# plt.legend(loc=2)
-# plt.show()
